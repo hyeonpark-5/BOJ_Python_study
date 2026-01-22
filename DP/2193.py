c2f0db0355540494b45e3f25cdce3862d11f9031
hyeonpark-5 <sorkgus@gmail.com>
2026-01-22 21:36:43 +0900

    2193

---
+++ DP/2193.py
@@ -0,0 +1,12 @@
+n = int(input())
+dp = [0] * n
+
+if n == 1 or n == 2:
+    print(1)
+else:
+    dp[0] = 1
+    dp[1] = 1
+    for i in range(2, n):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    
+    print(dp[-1])
